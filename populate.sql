021cf1852c4e1ed571a21cb832b2f8725d62883a
CarlosVigario <49401519+CarlosVigario@users.noreply.github.com>
2020-01-22 12:56:54 +0000

    Ficheiros para criar a base de dados

--- populate.sql
+++ populate.sql
@@ -1,88 +1,74 @@
-/* Tabela Category*/
-INSERT INTO Category (name)
-VALUES('Adventure');
 
-/* Tabela Catalogue */
-INSERT INTO Catalogue  (idCatalogue, name, description, dateAdded, lastModified)
-VALUES(0, 'The Adventures of Tintin', 'Classic Belgian comic written by Herge about the reporter Tintin', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+--
+-- Tabela Catalogue
+--
+INSERT INTO `Catalogue` (`idCatalogue`, `name`, `description`, `dateAdded`, `lastModified`) VALUES
+(0, 'The Adventures of Tintin', 'Classic Belgian comic written by Herge about the reporter Tintin', '2019-12-11 11:13:35', '2019-12-11 11:13:35'),
+(1, 'Blacksad', 'Private detective solves crime in 1950 america', '2020-01-09 08:59:08', '2020-01-09 08:59:08');
 
-/* Tabela Item */
-INSERT INTO Item (idItem, name, catalogueId, description, category)
-VALUES(0, 'Tintin in the Land of the Soviets', 0, 'Tintin is sent to the Soviet Union', 'Adventure');
+--
+-- Tabela Category
+--
+INSERT INTO `Category` (`name`) VALUES
+('Adventure'),
+('Criminal');
 
-INSERT INTO Item (idItem, name, catalogueId, description, category)
-VALUES(1, 'Tintin the Congo', 0, 'Tintin is sent to the Congo','Adventure');
+--
+-- Tabela User
+--
+INSERT INTO `User` (`userId`, `userEmail`, `reputation`, `password`) VALUES
+(0, 'carlwork@example.com', 0, '1234'),
+(1, 'tomasand@example.com', 0, '4321'),
+(2, 'luis82@example.com', 0, '7532');
 
-INSERT INTO Item (idItem, name, catalogueId, description, category)
-VALUES(2, 'Tintin in America', 0, 'Tintin has an adventure in America','Adventure');
-
-/* Tabela Edition */
-INSERT INTO Edition (idEdition, name, publisher, langCode, idItem, publicationDate)
-VALUES(0,  'Tintin in the Land of the Soviets', 'Sundancer', 'EN', 0, 1989);
-
-INSERT INTO Edition (idEdition, name, publisher, langCode, idItem, publicationDate)
-VALUES(1, 'Tintin au Congo', 'Le Petit Vingtième', 'FR', 1, 1931);
-
-INSERT INTO Edition (idEdition, name, publisher, langCode, idItem, publicationDate)
-VALUES(2, 'Tintin in America', 'Le Petit Vingtième', 'FR', 2, 1932);
-
-/* Tabela ItemCategory */
-INSERT INTO itemCategory (categoryName, itemId)
-Values ('Adventure', 0);
-
-
-/* Tabela User */
-INSERT INTO User (userId, userEmail, reputation, password)
-Value (0, 'carlwork@example.com', 0, 1234);
-
-INSERT INTO User (userId, userEmail, reputation, password)
-Value (1, 'tomasand@example.com', 0, 4321);
-
-INSERT INTO User (userId, userEmail, reputation, password)
-Value (2, 'luis82@example.com', 0, 7532);
-
-/* Tabela Collection */
-
-Insert INTO Collection (idCollection, user, catalogue)
-Value (0, 0, 0);
-
-Insert INTO Collection (idCollection, user, catalogue)
-Value (1, 1, 0);
-
-Insert INTO Collection (idCollection, user, catalogue)
-Value (2, 2, 0);
+--
+-- Tabela Collection
+--
+INSERT INTO `Collection` (`idCollection`, `user`, `catalogue`) VALUES
+(0, 0, 0),
+(1, 1, 0),
+(2, 2, 0);
 
 
-/* Tabela userBook */
+--
+-- Tabela Item
+--
+INSERT INTO `Item` (`idItem`, `name`, `catalogueId`, `description`, `category`) VALUES
+(0, 'Tintin in the Land of the Soviets', 0, 'Tintin is sent to the Soviet Union', 'Adventure'),
+(1, 'Tintin the Congo', 0, 'Tintin is sent to the Congo', 'Adventure'),
+(2, 'Tintin in America', 0, 'Tintin has an adventure in America', 'Adventure'),
+(4, 'Somewhere Within the Shadows', 1, 'Actress Natalia Willfred has been murdered', 'Criminal'),
+(5, 'Artic Nation', 1, 'Lynching and racially motivated crime is on the rise', 'Criminal'),
+(6, 'Red Soul', 1, 'Blacksad is employed has a bodyguard', 'Criminal');
 
-/*Carlos*/
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (0, 0, 0, 0, 'The book is in a perfect shape');
+--
+-- Tabela Edition
+--
+INSERT INTO `Edition` (`idEdition`, `name`, `publisher`, `langCode`, `idItem`, `publicationDate`, `image`) VALUES
+(0, 'Tintin in the Land of the Soviets', 'Sundancer', 'EN', 0, 1989, NULL),
+(1, 'Tintin au Congo', 'Le Petit Vingtième', 'FR', 1, 1931, NULL),
+(2, 'Tintin in America', 'Le Petit Vingtième', 'FR', 2, 1932, NULL),
+(3, 'Somewhere Within the Shadows', 'Dark Horse', 'EN', 4, 2000, NULL),
+(4, 'Artic Nation', 'Dark Horse', 'EN', 5, 2002, NULL),
+(5, 'Red Soul', 'Dark Horse', 'EN', 6, 2003, NULL);
 
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (1, 1, 0, 1, 'There is a page with a stain');
+--
+-- Tabela ItemCategory
+--
+INSERT INTO `itemCategory` (`categoryName`, `itemId`) VALUES
+('Adventure', 0),
+('Criminal', 1);
 
-/*Tomas*/
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (2, 0, 1, 0, 'Book with some stains');
-
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (3, 1, 1, 1, 'Book in a perfect shape');
-
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (4, 2, 1, 2, 'Page 12 is missing');
-
-/*Luis*/
-
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (5, 0, 2, 0, 'Book is in a perfect shape');
-
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (6, 1, 2, 1, 'Book is in a perfect shape');
-
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (7, 2, 2, 2, 'Book is in a perfect shape');
-
-INSERT INTO userBook (bookId, idItem, idCollection, idEdition, description)
-Value (8, 2, 2, 2, 'Book is in a perfect shape');
-
+--
+-- Tabela userBook
+--
+INSERT INTO `userBook` (`bookId`, `idItem`, `idCollection`, `idEdition`, `description`) VALUES
+(0, 0, 0, 0, 'The book is in a perfect shape'),
+(1, 1, 0, 1, 'There is a page with a stain'),
+(2, 0, 1, 0, 'Book with some stains'),
+(3, 1, 1, 1, 'Book in a perfect shape'),
+(4, 2, 1, 2, 'Page 12 is missing'),
+(5, 0, 2, 0, 'Book is in a perfect shape'),
+(6, 1, 2, 1, 'Book is in a perfect shape'),
+(7, 2, 2, 2, 'Book is in a perfect shape'),
+(8, 2, 2, 2, 'Book is in a perfect shape');
