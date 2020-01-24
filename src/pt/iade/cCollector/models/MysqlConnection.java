b68a3e684339868e96699c8ff25c9bb52eec60d4
Caradepato <49392474+Caradepato@users.noreply.github.com>
2020-01-24 14:01:49 +0000

    Update MysqlConnection.java
    Changed connection to production

--- src/pt/iade/cCollector/models/MysqlConnection.java
+++ src/pt/iade/cCollector/models/MysqlConnection.java
@@ -7,12 +7,12 @@ import java.sql.SQLException;
 /** handles the connection to the database **/
 
 public class MysqlConnection {
-	/*private static final String URL = "jdbc:mysql://remotemysql.com:3306/Gtwkt5USSe?useSSL=false";
+	private static final String URL = "jdbc:mysql://remotemysql.com:3306/Gtwkt5USSe?useSSL=false";
 	private static final String USER = "Gtwkt5USSe";
-	private static final String PASS = "OxAMLiPad8";*/
-	private static final String URL = "jdbc:mysql://remotemysql.com:3306/eBUrTlCHMB?useSSL=false";
+	private static final String PASS = "OxAMLiPad8";
+	/*private static final String URL = "jdbc:mysql://remotemysql.com:3306/eBUrTlCHMB?useSSL=false";
 	private static final String USER = "eBUrTlCHMB";
-	private static final String PASS = "GQkutlzVyF";
+	private static final String PASS = "GQkutlzVyF";*/
 	
 	private static Connection connection = null;
 	
